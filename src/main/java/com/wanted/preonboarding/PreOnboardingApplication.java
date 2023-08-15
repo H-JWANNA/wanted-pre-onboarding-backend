b5b4d396214285569d5f7e99285b4ad658a97160
Hong Jeongwan <hongjjwan@gmail.com>
2023-08-15 21:02:47 +0900

    환경변수 및 의존성 수정 (#1)
    * build: MySQL 의존성 변경
    
    * feat: 환경 변수 사용을 위한 env.properties 설정

--- src/main/java/com/wanted/preonboarding/PreOnboardingApplication.java
+++ src/main/java/com/wanted/preonboarding/PreOnboardingApplication.java
@@ -2,8 +2,10 @@ package com.wanted.preonboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/env.properties")
 public class PreOnboardingApplication {
 
 	public static void main(String[] args) {
