308909edba52f88d36b250a5408bd12fbb811459
Hong Jeongwan <hongjjwan@gmail.com>
2023-08-16 23:26:28 +0900

    Post Column nullable 속성 변경 (#12)
    * feat: Post Column에 null값 불가능 하게 변경
    
    * feat: Patch Request에는 nullable로 설정

--- src/main/java/com/wanted/preonboarding/board/PostRequest.java
+++ src/main/java/com/wanted/preonboarding/board/PostRequest.java
@@ -1,7 +1,15 @@
 package com.wanted.preonboarding.board;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.NonNull;
+
 public record PostRequest(
+	@NonNull
+	@NotBlank(message = "제목을 작성해주세요.")
 	String title,
+
+	@NonNull()
+	@NotBlank(message = "내용을 작성해주세요.")
 	String content
 ) {
 }
---
+++ src/main/java/com/wanted/preonboarding/board/PostUpdateRequest.java
@@ -0,0 +1,7 @@
+package com.wanted.preonboarding.board;
+
+public record PostUpdateRequest(
+	String title,
+	String content
+) {
+}
