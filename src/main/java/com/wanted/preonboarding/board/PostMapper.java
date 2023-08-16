d65d6bca302de8d169c9d1ece9e9cc3c563717ef
H-JWANNA <hongjjwan@gmail.com>
2023-08-16 18:32:30 +0900

    feat: 게시글 Mapper 구현

---
+++ src/main/java/com/wanted/preonboarding/board/PostMapper.java
@@ -0,0 +1,18 @@
+package com.wanted.preonboarding.board;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import com.wanted.preonboarding.member.Member;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PostMapper {
+
+	@Mapping(source = "dto.title", target = "title")
+	@Mapping(source = "dto.content", target = "content")
+	@Mapping(source = "member.memberId", target = "author")
+	Post toEntity(PostRequest dto, Member member);
+
+	PostResponse toResponse(Post entity);
+}
