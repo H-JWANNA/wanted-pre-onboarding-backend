9a6a25f3cc6f5f16ef61136a4627d3b728cc6ef9
H-JWANNA <hongjjwan@gmail.com>
2023-08-16 20:41:28 +0900

    feat: 회원 이메일 속성 unique로 변경

--- src/main/java/com/wanted/preonboarding/member/Member.java
+++ src/main/java/com/wanted/preonboarding/member/Member.java
@@ -21,7 +21,7 @@ public class Member {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long memberId;
 
-	@Column(name = "email", nullable = false)
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 
 	@Column(name = "password", nullable = false)
